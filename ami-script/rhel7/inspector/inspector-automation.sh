df6460634bb9d6fad3332915daf765b1b09d5d38
laeticiah <92341948+laeticiah@users.noreply.github.com>
2024-04-28 08:53:34 -0700

    Update inspector-automation.sh

--- ami-script/rhel7/inspector/inspector-automation.sh
+++ ami-script/rhel7/inspector/inspector-automation.sh
@@ -1,2 +1,2 @@
 /usr/bin/aws s3 create-cp s3://inspector-agent-us-west-2/linux/install.sh .
-tag = aws ec2 describe-tags --filters "Name=resource-id,Values=$(curl -s http://
+tag = /usr/local/bin/aws inspector create-resource-group --resource-group-tag tag-name 
